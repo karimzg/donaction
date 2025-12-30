bff8cc347c7dcdfd7d49ba33c6fe87319722f5b2
karimzg <k.zgoulli@gmail.com>
2025-12-30 20:02:47 +0100

    feat(logging): add debug logs to access token and API key retrieval functions

--- donaction-api/src/helpers/gcc/getAccessToken.ts
+++ donaction-api/src/helpers/gcc/getAccessToken.ts
@@ -3,12 +3,15 @@ import {GoogleAuth} from "google-auth-library";
 export default async function getAccessToken(): Promise<string> {
   return new Promise(async (resolve, reject) => {
     try {
+        console.log("000 - getAccessToken");
       const auth = new GoogleAuth({
         scopes: "https://www.googleapis.com/auth/cloud-platform",
       });
-
+        console.log("000 - getAccessToken", auth);
       const client = await auth.getClient();
+        console.log("001 - getAccessToken", client);
       const res = await client.getAccessToken();
+        console.log("002 - getAccessToken", res?.token);
 
       resolve(res?.token);
     } catch (e) {
